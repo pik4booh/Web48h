f99cc2cbaeffa14ee03599a4e32f48dd4ed0ea14
Miandritiana <miandrititana21@gmail.com>
2023-07-10 09:05:39 +0300

    base profil front office

--- base.sql
+++ base.sql
@@ -1,9 +1,23 @@
-CREATE dataBase; 
-use dataBase;
+CREATE database regime; 
+use regime;
 
 CREATE TABLE `user` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `nom` VARCHAR(20),
   `email` VARCHAR(50),
   `mdp` VARCHAR(20)
+);
+
+CREATE table objectif (
+  idOjectif int AUTO_INCREMENT PRIMARY key,
+  nom VARCHAR(100)
+);
+
+CREATE TABLE profilUser (
+  idProfilUser int PRIMARY key AUTO_INCREMENT,
+  idUser int references user(id),
+  genre char not null,
+  taille decimal,
+  poids decimal,
+  idOjectif int references objectif(idOjectif)
 );
