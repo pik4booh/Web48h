d55ae65830a972f65ce744ae73d0d58bbee413d3
pik4booh <124702206+koloinaAP@users.noreply.github.com>
2023-07-10 12:40:22 +0300

    Update base.sql
    zay

--- base.sql
+++ base.sql
@@ -21,3 +21,27 @@ CREATE TABLE profilUser (
   poids decimal,
   idOjectif int references objectif(idOjectif)
 );
+ CREATE table régime(
+  idrégime int PRIMARY key AUTO_INCREMENT,
+  nom VARCHAR ,
+  durée INTERVAL ,
+  prix numeric 
+ );
+ create table sport(
+  idsport int primary key AUTO_INCREMENT,
+  nomsport varchar,
+  durée INTERVAL,
+  calorie very DECIMAL,
+  prise de masse DECIMAL
+ );
+
+ insert into 'user'('nom','email','mdp') values ('johnson_alice','alice.johnson@gmail.com','johnson132');
+ insert into 'user'('nom','email','mdp') values ('Emily_davis','davis.emily@gmail.com','emily456');
+ insert into 'user'('nom','email','mdp') values ('David_Wilson',' david.wilson@gmail.com','wilson789');
+ insert into 'user'('nom','email','mdp') values ('John_Smith',' john.smith@gmail.com','smith321');
+ insert into 'user'('nom','email','mdp') values ('Sarah_key','sarah.key@gmail.com','key654');
+
+ insert into 'objectif'('idOjectif','nom') values ('perte de poids');
+ insert into 'objectif'('idOjectif','nom') values ('prise de poids');
+ 
+ insert into 'profilUser'(' idProfilUser','idUser',' genre','taille','poids',)values();
