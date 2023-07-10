9f0bbd8a682bf61013518168446b2875c4e31bf9
Kenny101001 <kennylandhy@gmail.com>
2023-07-10 09:08:35 +0300

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -4,12 +4,11 @@
   <title>Connexion</title>
 </head>
 <body>
-  <?php echo form_open('login/login'); ?>
+    
   <form action="<?php echo base_url('controllers/login/login');?>"></form>
     <input type="text" name="nom" placeholder="Nom d'utilisateur" required><br>
     <input type="password" name="mdp" placeholder="Mot de passe" required><br>
     <button type="submit">Se connecter</button>
-  <?php echo form_close(); ?>
   
   <?php if (isset($error)): ?>
     <p><?php echo $error; ?></p>
